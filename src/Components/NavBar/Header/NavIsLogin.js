552f62b6508dbd6c462e8f933c7114c2e7e21c27
Ardian ANG <ardianshalihin1@gmail.com>
2022-01-26 16:56:23 +0800

    coba navbarLogin

--- src/Components/NavBar/Header/NavIsLogin.js
+++ src/Components/NavBar/Header/NavIsLogin.js
@@ -1,13 +1,15 @@
 import avatar from "../../Assets/icons/avatar.png";
 import { Link } from "react-router-dom";
-import { useSelector } from "react-redux";
+// import { useSelector } from "react-redux";
 
 
 export default function NavIsLogin() {
-  const { token } = useSelector((state) => state);
+  const isLogin = localStorage.getItem("token") ? true : false;
+
+  // const { token } = useSelector((state) => state);
 
 
-  if (token) {
+  if (isLogin) {
     return (
       <a className="navbar-brand" href="/profile">
         <img alt="logo SeeEvent" src={avatar} width="auto" height="auto" />
